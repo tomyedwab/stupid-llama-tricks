529b203a409f9eb88a6462941a301fe835168149
Tom W. Yedwab <sincerelyyourstom@gmail.com>
2024-11-08 17:26:57 -0800

    Full editing support for branches

--- client/js/operation-editor.js
+++ client/js/operation-editor.js
@@ -20,6 +20,9 @@ class OperationEditor extends HTMLDivElement {
         this.appendChild(templateContent.cloneNode(true));
 
         this.onUpdate = () => {};
+        this.onAdd = () => {};
+        this.onRemove = () => {};
+
         this.checkValid();
 
         this.setId(id);
@@ -31,6 +34,16 @@ class OperationEditor extends HTMLDivElement {
             this.checkValid();
             });
         });
+
+        this.querySelector(".add").addEventListener("click", () => {
+            this.onAdd(this.id, null);
+        });
+        this.querySelector(".remove").addEventListener("click", () => {
+            this.onRemove(this.id);
+        });
+        this.querySelector(".help").addEventListener("click", () => {
+            // TODO: Show help text somewhere
+        });
     }
 
     setId(id) {
@@ -41,6 +54,9 @@ class OperationEditor extends HTMLDivElement {
     setRole(role) {
         this.role = role;
         this.querySelector("input[name=role][value=" + role + "]").checked = true;
+    }
+
+    forEachChild(callback) {
     }
 }
 
@@ -63,7 +79,6 @@ class OperationEditorText extends OperationEditor {
         this.valid = this.parameters["tokenized"].length > 0;
         this.onUpdate();
     }
-
 
     onTextInput(value) {
         this.parameters["raw"] = value;
@@ -152,27 +167,201 @@ class OperationEditorCompletion extends OperationEditor {
     }
 }
 
+class OperationEditorChildOperations extends HTMLDivElement {
+    constructor(id, parameters, onUpdate, onAdd, onRemove) {
+        super();
+        this.id = id;
+        this.operations = [];
+        this.parameters = {
+            "operations": [],
+        };
+        this.onUpdate = onUpdate;
+        this.onAdd = onAdd;
+
+        this.classList.add("child-operations");
+        this.classList.add("visible-branch");
+        this.setAttribute("data-id", id);
+
+        if (parameters.operations.length > 0) {
+            for (const [type, role, child_parameters] of parameters.operations) {
+                if (type === "text") {
+                    this.addOperation(new OperationEditorText(0, role, child_parameters));
+                } else if (type === "completion") {
+                    this.addOperation(new OperationEditorCompletion(0, role, child_parameters));
+                } else if (type === "branch") {
+                    this.addOperation(new OperationEditorBranch(0, role, child_parameters));
+                }
+            }
+        } else {
+            const newChild = new OperationEditorText(0, "assistant");
+            this.addOperation(newChild);
+        }
+    }
+
+    setId(id) {
+        this.id = id;
+        this.setAttribute("data-id", id);
+    }
+
+    isValid() {
+        for (const operation of this.operations) {
+            if (!operation.valid) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    addOperation(operation, afterOperationId) {
+        operation.onUpdate = () => {
+            this.onUpdate();
+        };
+        operation.onAdd = (id, parent) => {
+            this.onAdd(id, parent || this);
+        };
+        operation.onRemove = (id) => {
+            this.removeOperation(id);
+        };
+        if (afterOperationId) {
+            const index = this.operations.findIndex(operation => operation.id === afterOperationId) + 1;
+            this.operations.splice(index, 0, operation);
+            this.parameters.operations.splice(index, 0, [operation.type, operation.role, operation.parameters]);
+            this.insertBefore(operation, this.children[index]);
+        } else {
+            this.operations.push(operation);
+            this.parameters.operations.push([operation.type, operation.role, operation.parameters]);
+            this.appendChild(operation);
+        }
+        this.onUpdate();
+    }
+
+    removeOperation(id) {
+        const index = this.operations.findIndex(operation => operation.id === id);
+        this.operations.splice(index, 1);
+        this.parameters.operations.splice(index, 1);
+        this.removeChild(this.children[index]);
+        this.onUpdate();
+    }
+}
+
 class OperationEditorBranch extends OperationEditor {
     constructor(id, defaultRole, params) {
         super(id, "branch", defaultRole, "operation-editor-branch", params || {
             "options": [],
         });
+
+        this.options = [];
+
+        if (this.parameters["options"].length > 0) {
+            for (const option of this.parameters["options"]) {
+                const childOperations = new OperationEditorChildOperations(0, option, () => {
+                    this.onChildrenUpdate();
+                }, (idx, parent) => {
+                    this.onAdd(idx, parent);
+                }, (idx) => {
+                    this.onRemove(idx);
+                });
+                this.addOption(childOperations);
+            }
+        } else {
+            const newOption = new OperationEditorChildOperations(0, {
+                "operations": [],
+            }, () => {
+                this.onChildrenUpdate();
+            }, (idx, parent) => {
+                this.onAdd(idx, parent);
+            }, (idx) => {
+                this.onRemove(idx);
+            });
+            this.addOption(newOption);
+        }
+
+        this.querySelector(".add-branch").addEventListener("click", () => {
+            const newOption = new OperationEditorChildOperations(0, {
+                "operations": [],
+            }, () => {
+                this.onChildrenUpdate();
+            }, (idx, parent) => {
+                this.onAdd(idx, parent);
+            }, (idx) => {
+                this.onRemove(idx);
+            });
+            this.addOption(newOption);
+            this.onChildrenUpdate();
+        });
+        this.querySelector(".remove-branch").addEventListener("click", () => {
+            // Get the selected branch
+            const selectedBranch = parseInt(this.querySelector("input[name=selected-branch]:checked").value);
+            this.removeOption(selectedBranch - 1);
+            this.onChildrenUpdate();
+        });
+
+        this.updateIds();
+        this.onChildrenUpdate();
+        this.checkValid();
     }
 
-    checkValid() {
-        this.valid = this.parameters["branch"]["options"].length > 0;
+    onChildrenUpdate() {
+        this.parameters["options"] = this.options.map(option => option.parameters);
         this.onUpdate();
     }
 
-    updateBranchOptions(options) {
-        // TODO: Show tokenized indicator
-        this.parameters["branch"]["options"] = options;
-        let html = "";
-        for (let idx = 0; idx < options.length; idx++) {
-            html += `<label>Option ${idx+1}: <input name="branch-${idx}" value="${options[idx]["raw"]}" /></label>`;
+    updateIds() {
+        for (let i = 0; i < this.options.length; i++) {
+            this.options[i].setId(i + 1);
         }
-        this.querySelector("div.type-parameters[data-type=branch]").innerHTML = html;
-        this.checkValid();
+        const selectorParent = this.querySelector(".branch-selector");
+        for (let i = 0; i < selectorParent.children.length; i++) {
+            selectorParent.children[i].querySelector("input").value = (i + 1).toString();
+        }
+        if (!this.querySelector("input[name=selected-branch]:checked")) {
+            selectorParent.children[0].querySelector("input").checked = true;
+        }
+    }
+
+    checkValid() {
+        if (this.options === undefined) {
+            this.valid = false;
+        } else if (this.options.length === 0) {
+            this.valid = false;
+        } else {
+            this.valid = true;
+            for (const option of this.options) {
+                if (!option.isValid()) {
+                    this.valid = false;
+                    break;
+                }
+            }
+        }
+        this.onUpdate();
+    }
+
+    forEachChild(callback) {
+        for (const option of this.options) {
+            for (const operation of option.operations) {
+                callback(operation);
+            }
+        }
+    }
+
+    addOption(childOperations) {
+        this.options.push(childOperations);
+        this.appendChild(childOperations);
+
+        const index = this.options.length;
+        const selectorParent = this.querySelector(".branch-selector");
+        const selector = document.createElement("label");
+        selector.innerHTML = `<input type="radio" name="selected-branch" value="${index}" checked>${index}`;
+        selectorParent.appendChild(selector);
+        this.updateIds();
+    }
+
+    removeOption(index) {
+        this.options[index].remove();
+        this.options.splice(index, 1);
+        const selectorParent = this.querySelector(".branch-selector");
+        selectorParent.removeChild(selectorParent.children[index]);
+        this.updateIds();
     }
 
     onSubmit() {
@@ -180,17 +369,12 @@ class OperationEditorBranch extends OperationEditor {
             return null;
         }
         let forks = [];
-        for (let idx = 0; idx < this.parameters["options"].length; idx++) {
-            forks.push([
-                {
-                    id: `${this.id}:${idx}`,
-                    name: "feed_tokens",
-                    feed_tokens: {
-                        tokens: this.parameters["options"][idx]["tokenized"],
-                        top_p: 10,
-                    },
-                },
-            ]);
+        for (const option of this.options) {
+            let optionForks = [];
+            for (const operation of option.operations) {
+                optionForks.push(operation.onSubmit());
+            }
+            forks.push(optionForks);
         }
         return {
             id: `${this.id}`,
@@ -206,3 +390,4 @@ customElements.define("operation-editor", OperationEditor, { extends: "div" });
 customElements.define("operation-editor-text", OperationEditorText, { extends: "div" });
 customElements.define("operation-editor-completion", OperationEditorCompletion, { extends: "div" });
 customElements.define("operation-editor-branch", OperationEditorBranch, { extends: "div" });
+customElements.define("operation-editor-child-operations", OperationEditorChildOperations, { extends: "div" });
--- client/main.js
+++ client/main.js
@@ -3,7 +3,8 @@ class AppState {
         this.blocks = [];
         this.blocksByID = {};
         this.results = null;
-
+        this.addBlockNextId = null;
+        this.addBlockParent = null;
         const serialized = localStorage.getItem("state");
         if (serialized) {
             const state = JSON.parse(serialized);
@@ -35,19 +36,83 @@ class AppState {
                 this.results.handleWordSelection(null, false);
             }
         });
+
+        document.getElementById('add-operation-dialog').querySelector('button.ok').addEventListener("click", () => {
+            this.onAddBlockDialogSubmit();
+        });
     }
 
-    addBlock(block) {
+    addBlock(block, afterBlockId) {
         block.onUpdate = () => {
             this.onUpdate();
         };
-        this.blocks.push(block);
+        block.onAdd = (id, parent) => {
+            this.onAddBlock(id, parent);
+        };
+        block.onRemove = (id) => {
+            this.onRemoveBlock(id);
+        };
+
+        const conversationBlocks = document.getElementById("conversation-blocks");
+        if (afterBlockId) {
+            const index = this.blocks.findIndex(block => block.id === afterBlockId) + 1;
+            this.blocks.splice(index, 0, block);
+            conversationBlocks.insertBefore(block, conversationBlocks.children[index]);
+        } else {
+            this.blocks.push(block);
+            conversationBlocks.appendChild(block);
+        }
         this.blocksByID[block.id] = block;
-        document.getElementById("conversation-blocks").appendChild(block);
+    }
+
+    onAddBlock(blockId, parent) {
+        this.addBlockNextId = blockId;
+        this.addBlockParent = parent;
+        document.getElementById('add-operation-dialog').showModal();
+    }
+
+    onAddBlockDialogSubmit() {
+        const dialog = document.getElementById('add-operation-dialog');
+        const type = dialog.querySelector("input[name=type]:checked").value;
+
+        let operation;
+        if (type === "text") {
+            operation = new OperationEditorText(0, "assistant");
+        } else if (type === "completion") {
+            operation = new OperationEditorCompletion(0, "assistant");
+        } else if (type === "branch") {
+            operation = new OperationEditorBranch(0, "assistant");
+        }
+
+        if (this.addBlockParent !== null) {
+            this.addBlockParent.addOperation(operation, this.addBlockNextId);
+        } else {
+            this.addBlock(operation, this.addBlockNextId);
+        }
+        this.onUpdate();
+    }
+
+    onRemoveBlock(blockId) {
+        const block = this.blocksByID[blockId];
+        delete this.blocksByID[blockId];
+        this.blocks = this.blocks.filter(block => block.id !== blockId);
+        document.getElementById("conversation-blocks").removeChild(block);
+        this.onUpdate();
     }
 
     onUpdate() {
         this.submitButton.disabled = !this.blocks.every(block => block.valid);
+
+        let nextBlockId = 1;
+        this.blocksById = {};
+        const _renumberBlock = (block) => {
+            block.setId(nextBlockId);
+            this.blocksByID[nextBlockId] = block;
+            nextBlockId++;
+            block.forEachChild(_renumberBlock);
+        };
+        this.blocks.forEach(_renumberBlock);
+
         const serialized = this.blocks.map(block => [block.type, block.role, block.parameters]);
         localStorage.setItem("state", JSON.stringify(serialized));
     }
